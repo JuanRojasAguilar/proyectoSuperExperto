2d6471775948830363bd1be42eedff813e120aad
Juan Rojas <yo217@hotmail.es>
2024-03-01 10:25:02 -0500

    menu_assets completed

--- modules/menu_assets.py
+++ modules/menu_assets.py
@@ -1,15 +1,20 @@
-from modules.corefiles import menus_layout, clear_screen, pause_screen
-import sys
 import csv
-import os
 import json
+import os
+import sys
+
 from tabulate import tabulate
+
+from modules.corefiles import clear_screen, menus_layout, pause_screen
+
 products_file = "productos_digitados.csv"
+
+
 # Cargar el archivo csv
 def read_productos_csv():
   data = []
   final_data = {}
-  with open("data/"+products_file, "r", encoding="utf-8") as file:
+  with open("data/" + products_file, "r", encoding="utf-8") as file:
     archive = csv.reader(file)
     for rows in archive:
       for row in rows:
@@ -19,69 +24,72 @@ def read_productos_csv():
           data.append(clean_row)
       for item in data:
         dicc = {
-          "NroItem": item[0],
-          "CodTransaccion": item[1],
-          "NroSerial": item[2],
-          "CodCampus": item[3],
-          "NroFormulario": item[4],
-          "Nombre": item[5],
-          "Ubicacion": item[6],
-          "Proveedor": item[7],
-          "EmpresaResponsable": item[8],
-          "Estado": item[9],
-          "codebar": item[10]
+            "NroItem": item[0],
+            "CodTransaccion": item[1],
+            "NroSerial": item[2],
+            "CodCampus": item[3],
+            "NroFormulario": item[4],
+            "Nombre": item[5],
+            "Ubicacion": item[6],
+            "Proveedor": item[7],
+            "EmpresaResponsable": item[8],
+            "Estado": item[9],
+            "codebar": item[10]
         }
         final_data.update({item[3]: dicc})
     final_data.pop("CodCampus")
     return final_data
 
+
 def check_file():
-    data = read_productos_csv()
-    try:
-      if os.path.isfile("data/actives.json"):
-        return
-      else:
-        with open("data/actives.json", "w", encoding="utf-8") as file:
-          json.dump(data, file, indent=2, ensure_ascii=False)
-          file.close()
-    except:
-        sys.exit("Problemitas")
+  data = read_productos_csv()
+  try:
+    if os.path.isfile("data/actives.json"):
+      return
+    else:
+      with open("data/actives.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+        file.close()
+  except:
+    sys.exit("Problemitas")
 
 def search_asset(data):
   res = {}
-  searchedAsset = input("Por favor ingrese el codigo del producto dado por campus: ").upper()
-  for key, val in data.items():
-    if key == searchedAsset:
+  searchedAsset = input()
+  for val in data.values():
+    if val["codebar"] == searchedAsset.encode("utf-8"):
       res = val
       break
     else:
       res = False
   return res
 
+
 def show_search_result(data):
   res = search_asset(data)
   print(tabulate([res], headers="keys", tablefmt="grid"))
   input("\nPresione ENTER para volver...")
 
+
 def update(data):
   with open("data/actives.json", "w") as file:
     json.dump(data, file, indent=2, ensure_ascii=False)
     file.close()
-  
+
 
 def edit(data):
   res = search_asset(data)
   print(res)
 
-def delete(data):
- res = search_asset(data)
- name = res.get("Nombre").strip()
- id = res.get("CodCampus")
- data.pop(res["CodCampus"])
- update(data)
- print(f"El objeto {name} de código {id} ha sido eliminado")
- pause_screen()
 
+def delete(data):
+  res = search_asset(data)
+  name = res.get("Nombre").strip()
+  id = res.get("CodCampus")
+  data.pop(res["CodCampus"])
+  update(data)
+  print(f"El objeto {name} de código {id} ha sido eliminado")
+  pause_screen()
 
 
 def menu_assets():
@@ -92,7 +100,8 @@ def menu_assets():
   +  MENU ACTIVOS +
   +++++++++++++++++
   """
-  menu = [["1.", "Agregar"],["2.", "Editar"],["3.", "Eliminar"],["4.", "Buscar"],["5.", "Regresar al menu principal"]]
+  menu = [["1.", "Agregar"], ["2.", "Editar"], ["3.", "Eliminar"],
+          ["4.", "Buscar"], ["5.", "Regresar al menu principal"]]
   menus_layout(title, menu)
   option = input("\n>> ")
   if option == "1":
@@ -111,28 +120,3 @@ def menu_assets():
   else:
     menu_assets()
 
-
-
-
-
-"""
-active={
-  'CodTransaccion': '',
-  'NoFormulario': '',
-  'CodCampus': '',
-  'Marca': '',
-  'Categoria': '',
-  'Tipo': '',
-  'ValorUnitario': '',
-  'proveedor': '',
-  'NoSerial': '',
-  'EmpresaResponsable': '',
-  'Estado': '',
-  'Historial':{
-    'Nrold': '',
-    'Fecha': '',
-    'TipoMovimiento': '',
-    'IdRespMov': '',
-  }
-}
-"""
