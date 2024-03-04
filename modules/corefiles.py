c2d555455f1b0305a17a342a2be48bbcee11588a
Alejo0507 <castillocazorla05@gmail.com>
2024-03-04 16:06:50 -0500

    mejora a menu reportes

--- modules/corefiles.py
+++ modules/corefiles.py
@@ -4,6 +4,7 @@ import json
 from tabulate import tabulate
 
 
+
 # Este es el layout de los menús, imprime el titulo y el menu
 def menus_layout(title:str,menu:list):
   print(title)
@@ -41,12 +42,7 @@ assets={}
 def json_assets(assets):
     with open('data/assets.json',"w") as ac:
         json.dump(assets,ac, indent=4)
-#para los activos hay 4 estador 0 no asignado 1 asignado 2 dado de baja por daño 3 en repacion y/o garantia
-#Creo una funcion donde filtro el estado deseado de los activos
-#damaged_assets = [asset for asset in assets.values() if asset['Estado'] == 2]
-#filtro categorias dentro de activos
-#campus_codes= [asset['CodCampus'] for asset in assets.values()]
-
+#lista todos los activos
 def list_Assets():
   list=[]
   data = check_json("data/assets.json", {})
@@ -63,14 +59,135 @@ def list_Assets():
     all_page=(len(list)-1)//pages+1
     for idx in range(all_page):
       sub_data=list[idx*pages:(idx+1)*pages]
-      print(tabulate(sub_data, headers=["CODIGO","TIPO","NOMBRE","No.SERIAL"],tablefmt="fancy_grid"))
-      print(f'Pagina {idx+1} de {pages}')
+      print(tabulate(sub_data, headers=["CODIGO","NOMBRE","TIPO","No.SERIAL"],tablefmt="fancy_grid"))
+      print(f'Pagina {idx+1} de {all_page}')
       op =input('si desea volver presione (1)')
       if op == "1":
         break
   else:
     print('aun no hay archivos')      
-    
-    
-    
-
+#lista los activos por categoria
+def list_Category():
+  data=check_json("data/assets.json", {})
+  while True:
+    clear_screen()
+    opcion = (""" 
+    1. Listar categoria equipos de computo 
+    2. Listar categoria juegos
+    3. Listar categoria electrodomesticos
+    4. Volver al menu de reportes
+                  """)
+    print(opcion)
+    op = input(">> ")
+    if op == "1":
+      list=[]
+      for key,value in data.items():
+        code=key
+        if data[key]["categoria"]=="Equipo de computo":
+          category=data[key]["categoria"]
+          name=data[key]["Nombre"]
+          num_serial=data[key]["NroSerial"]
+          sub_list=[code,category,name,num_serial]
+          list.append(sub_list)
+      if list:
+        pages=30
+        all_page=(len(list)-1)//pages+1
+        for idx in range(all_page):
+            sub_data=list[idx*pages:(idx+1)*pages]
+            print(tabulate(sub_data, headers=["CODIGO","CATEGORIA","NOMBRE","No.SERIAL"],tablefmt="fancy_grid"))
+            print(f'Pagina {idx+1} de {all_page}')
+            pause_screen()
+            op =input('si desea volver presione (1)')
+            
+            if op == "1":
+              clear_screen()
+              break
+      else:
+        print('aun no hay archivos')   
+        pause_screen()
+        clear_screen()     
+    elif op == "2":
+      list2=[]
+      for key,value in data.items():
+        code=key
+        if data[key]["categoria"]=="Juegos":
+          category=data[key]["categoria"]
+          name=data[key]["Nombre"]
+          num_serial=data[key]["NroSerial"]
+          sub_list=[code,category,name,num_serial]
+          list2.append(sub_list)
+      if list2:
+        pages=30
+        all_page=(len(list2)-1)//pages+1
+        for idx in range(all_page):
+            sub_data=list2[idx*pages:(idx+1)*pages]
+            print(tabulate(sub_data, headers=["CODIGO","CATEGORIA","NOMBRE","No.SERIAL"],tablefmt="fancy_grid"))
+            print(f'Pagina {idx+1} de {all_page}')
+            pause_screen()
+            op =input('si desea volver presione (1)')
+            
+            if op == "1":
+              clear_screen()
+              break
+      else:
+        print('aun no hay archivos')   
+        pause_screen()
+        clear_screen()
+    elif op == "3":
+      list3=[]
+      for key,value in data.items():
+        code=key
+        if data[key]["categoria"]=="Electrodomesticos":
+          category=data[key]["categoria"]
+          name=data[key]["Nombre"]
+          num_serial=data[key]["NroSerial"]
+          sub_list=[code,category,name,num_serial]
+          list3.append(sub_list)
+      if list3:
+        pages=30
+        all_page=(len(list2)-1)//pages+1
+        for idx in range(all_page):
+            sub_data=list3[idx*pages:(idx+1)*pages]
+            print(tabulate(sub_data, headers=["CODIGO","CATEGORIA","NOMBRE","No.SERIAL"],tablefmt="fancy_grid"))
+            print(f'Pagina {idx+1} de {all_page}')
+            pause_screen()
+            op =input('si desea volver presione (1)')
+            
+            if op == "1":
+              clear_screen()
+              break
+      else:
+        print('aun no hay archivos')   
+        pause_screen()
+        clear_screen()
+    elif op == "4":
+     break
+#lista los activos dados de baja por daño 
+def list_Broken_Assets():
+  data=check_json("data/assets.json", {})
+  list_Broke=[]
+  for key,value in data.items():
+        code=key
+        if data[key]["Estado"]=="Dado de baja por daño":
+          state=data[key]["Estado"]
+          name=data[key]["Nombre"]
+          num_serial=data[key]["NroSerial"]
+          sub_list=[code,state,name,num_serial]
+          list_Broke.append(sub_list)
+  if list_Broke:
+        pages=30
+        all_page=(len(list_Broke)-1)//pages+1
+        for idx in range(all_page):
+            sub_data=list_Broke[idx*pages:(idx+1)*pages]
+            print(tabulate(sub_data, headers=["CODIGO","ESTADO","NOMBRE","No.SERIAL"],tablefmt="fancy_grid"))
+            print(f'Pagina {idx+1} de {all_page}')
+            pause_screen()
+            op =input('si desea volver presione (1)')
+            
+            if op == "1":
+              clear_screen()
+              break
+  else:
+    print('aun no hay archivos')   
+    pause_screen()
+    clear_screen()
--- modules/menu_report.py
+++ modules/menu_report.py
@@ -12,24 +12,17 @@ def menu_report():
   menu = [["1.", "Listar todos los activos"],["2.", "Listar activos por categoria"],["3.", "Listar activos dados de baja por daño"],["4.", "Listar activos y asignacion"],["5.", "Listar historial de MOV. activos"],["6.", "Regresar al menu principal"]]
   cf.menus_layout(title, menu)
   option = input("\n>> ")
-  if option == "1": #Imprime el CodCampus de todos los asset en assets.
-    #for campus_codes in campus_codes:
-    #print(campus_codes)
+  if option == "1": 
     cf.list_Assets()
     menu_report()
-  elif option == "2": #Imprime los activos por categoria. FALTA
-    print("Activos por categoria :")
-  elif option == "3": #Imprime los activos con el estado 2, o dados de baja por daño.
-    #print(f"Activos dados de baja por daño:")
-    
-    #for asset in damaged_assets:
-      #print(f"{asset['CodCampus']}")
-    #x=input('continue')
-    pass
+  elif option == "2":
+    cf.list_Category()
+    menu_report()
+  elif option == "3":
+    cf.list_Broken_Assets()
+    menu_report()
   elif option == "4":
-    print("Activos y sus asignaciones de zonas correspondientes:") #Imprime lista de Tuplas, relacion zona-activo
-    for asset, zone in asset_zone:
-      print(f"Activo: {asset['CodCampus']} - Zona: {zone}")
+    pass
   elif option == "5":
     pass
   elif option == "6":
